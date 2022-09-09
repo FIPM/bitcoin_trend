be10cbc09b3061d20f5fc1a4cd6127474089545d
FIPM <fipm@iMac-de-FIPM.local>
2022-09-09 09:58:37 -0600

    added gitignore

---
+++ README.md
@@ -0,0 +1,48 @@
+# **Question:**
+
+With `monthly_data.csv`, `weekly_data.csv` and `hourly_data.csv` data files given to you by the engineering team, how do you use them to output **time series of normalized Google Trends data from 2017 till the present with time interval of hours that is relatively comparable/consistent**?
+
+
+## **Solution:**
+Task: we need to find a comparable/consistent normalized version of the values in the houly_data file.
+
+Let $n_{h,d,w,m,y}$ be the number of bitcoin searches in the hour $h$ of the day $d$ of the week $w$ of the month $m$ of the year $y$.
+
+We need to find $\frac{n_{h,d,w,m,y}}{n_{hourly}^{max}}$ where $n_{hourly}^{max}$ is the maximum value that $n_{h,d,w,m,y}$ can take in all the data, but each value we observe in the hourly_data file is given by $\frac{n_{h,d,w,m,y}}{n_{w,m,y}^{max}}$ where $n_{w,m,y}^{max}$ is the maximum value that $n_{h,d,w,m,y}$ can take in a particular $w,m,y$ tuple. 
+
+Definitions:
+
+1. Let $n_{w,m,y}=\sum_{d}\sum_{h}n_{h,d,w,m,y}$ be the number of bitcoin searches in the specific week $w$ of month $m$ of year $y$.
+
+    Adding up all elements of the hourly_data file by week, month, and year, we have:
+
+    $\frac{n_{w,m,y}}{n_{w,m,y}^{max}}=\frac{\sum_{d}\sum_{h}n_{h,d,w,m,y}}{n_{w,m,y}^{max}}$   $\left( Eq. 1 \right)$
+
+2. Let $n_{m,y}=\sum_{w}n_{w,m,y}$ be the number of bitcoin searches in the specific month $m$ of year $y$.
+
+    Adding up all elements of the weekly_data file by month, and year, we have
+
+    $\frac{n_{m,y}}{n_{y}^{max}}=\frac{\sum_{w}n_{w,m,y}}{n_{y}^{max}}$    $\left( Eq. 2 \right)$
+
+    where $n_{y}^{max}$ is the maximum value that $n_{w,m,y}$ can take in a particular year $y$.
+
+3. Each value we observe of the monthly_data file is given by $\frac{n_{m,y}}{n_{monthly}^{max}}$ where $n_{monthly}^{max}$ is the maximum value of $n_{m,y}$.
+
+
+
+Therefore, to find $\frac{n_{h,d,w,m,y}}{n_{hourly}^{max}}$ we need: 
+
+1. That for each value in the week_data file, $\frac{n_{w,m,y}}{n_{y}^{max}}$, using Eq. 2 we calculate:
+
+    $\frac{n_{w,m,y}}{n_{monthly}^{max}}=\frac{n_{w,m,y}}{n_{y}^{max}}\frac{n_{y}^{max}}{n_{m,y}}\frac{n_{m,y}}{n_{monthly}^{max}}$ $\left( Eq. 3 \right)$
+
+2. That for each value in the hourly_data file, $\frac{n_{h,d,w,m,y}}{n_{w,m,y}^{max}}$, using Eq. 1 and Eq. 3 we calculate:
+
+    $\frac{n_{h,d,w,m,y}}{n_{monthly}^{max}}=\frac{n_{h,d,w,m,y}}{n_{w,m,y}^{max}}\frac{n_{w,m,y}^{max}}{n_{w,m,y}}\frac{n_{w,m,y}}{n_{monthly}^{max}}$ $\left( Eq. 4 \right)$
+
+3. Let $\frac{n_{h,d,w,m,y}^{max}}{n_{monthly}^{max}}$ the maximum value that $\frac{n_{h,d,w,m,y}}{n_{monthly}^{max}}$ can take for all h,d,w,m,y. The comparable/consistent normalized version of the values in the houly_data file is then given by:
+
+    $\frac{n_{h,d,w,m,y}}{n_{hourly}^{max}}=\frac{\frac{n_{h,d,w,m,y}}{n_{monthly}^{max}}}{\frac{n_{h,d,w,m,y}^{max}}{n_{monthly}^{max}}}$ $\left( Eq. 5 \right)$
+
+# **Files:**
+To run the normalization_hourly_data.py file, the provided csv files  must be store inside a folder called raw_data. This folder has to be at the same level than the normalization_hourly_data.py file.
---
+++ normalization_hourly_data.py
@@ -0,0 +1,61 @@
+# Installs
+# pip install pandas
+
+# Imports
+import pandas as pd
+from datetime import date
+import os
+
+# Uploading data
+main_folder = os.getcwd()
+monthly_data = pd.read_csv(os.path.join(main_folder,'raw_data','monthly_data.csv'))
+weekly_data = pd.read_csv(os.path.join(main_folder,'raw_data','weekly_data.csv'))
+hourly_data = pd.read_csv(os.path.join(main_folder,'raw_data','hourly_data.csv'))
+
+# Creating key variables to merge datasets
+monthly_data['year'] = monthly_data['date'].apply(lambda x: date.fromisoformat(x).year)
+monthly_data['month'] = monthly_data['date'].apply(lambda x: date.fromisoformat(x).month)
+monthly_data['value_month']=monthly_data['value_month']/100
+
+weekly_data['year'] = weekly_data['date'].apply(lambda x: date.fromisoformat(x).year)
+weekly_data['month'] = weekly_data['date'].apply(lambda x: date.fromisoformat(x).month)
+weekly_data['iso_year'] = weekly_data['date'].apply(lambda x: date.fromisoformat(x).isocalendar()[0])
+weekly_data['iso_week'] = weekly_data['date'].apply(lambda x: date.fromisoformat(x).isocalendar()[1])
+weekly_data['value_week']=weekly_data['value_week']/100
+
+hourly_data['year'] = hourly_data['date'].apply(lambda x: date.fromisoformat(x.split(' ')[0]).year)
+hourly_data['month'] = hourly_data['date'].apply(lambda x: date.fromisoformat(x.split(' ')[0]).month)
+hourly_data['iso_year'] = hourly_data['date'].apply(lambda x: date.fromisoformat(x.split(' ')[0]).isocalendar()[0])
+hourly_data['iso_week'] = hourly_data['date'].apply(lambda x: date.fromisoformat(x.split(' ')[0]).isocalendar()[1])
+hourly_data['value_hour']=hourly_data['value_hour']/100
+
+# Renaming column in monthly_data
+db_n_m_y_monthly = monthly_data[['year','month','value_month']].rename(columns={'value_month':'n_m_y_montly'})
+
+# Calculating Equation 2
+db_n_m_y = weekly_data.groupby(['year','month'])[['value_week']].sum().rename(columns={'value_week':'sum_value_week'}).reset_index(drop=False)
+db_n_m_y['n_w_m_y'] = db_n_m_y['sum_value_week']
+db_n_m_y = db_n_m_y[['year','month','n_w_m_y']]
+
+# Calculating Equation 3
+db_n_w_m_y_montly = weekly_data.merge(db_n_m_y.merge(db_n_m_y_monthly, how='left', left_on=['year','month'], right_on=['year','month']),
+                  how='left', left_on=['year','month'], right_on=['year','month'])
+db_n_w_m_y_montly['n_w_m_y_montly'] = db_n_w_m_y_montly['value_week']/db_n_w_m_y_montly['n_w_m_y']*db_n_w_m_y_montly['n_m_y_montly']
+db_n_w_m_y_montly = db_n_w_m_y_montly[['iso_year','iso_week','n_w_m_y_montly']]
+
+# Calculation Equation 1
+db_n_w_m_y = hourly_data.groupby(['iso_year','iso_week'])[['value_hour']].sum().rename(columns={'value_hour':'n_w_m_y'}).reset_index(drop=False)
+db_n_h_d_w_m_y = hourly_data.merge(db_n_w_m_y.merge(db_n_w_m_y_montly, how='left', left_on=['iso_year','iso_week'], right_on=['iso_year','iso_week']),
+                  how='left', left_on=['iso_year','iso_week'], right_on=['iso_year','iso_week'])
+
+# Calculation Equation 4 (calculating the comparable/consistent version of the values in the houly_data file)
+db_n_h_d_w_m_y['n_h_d_w_m_y_monthly'] = db_n_h_d_w_m_y['value_hour']/db_n_h_d_w_m_y['n_w_m_y']*db_n_h_d_w_m_y['n_w_m_y_montly']
+
+# Calculation Equation 5 (Normalization)
+db_n_h_d_w_m_y['n_h_d_w_m_y'] = 100*db_n_h_d_w_m_y['n_h_d_w_m_y_monthly']/db_n_h_d_w_m_y['n_h_d_w_m_y_monthly'].max()
+db_n_h_d_w_m_y = db_n_h_d_w_m_y[['time_hour','date','n_h_d_w_m_y']]
+
+# There are some missing values because the weekly_data and hourly_data do not have the same number of iso weeks. 
+
+print('Maximum value of the consistent version of the values in the houly_data file: '+str(db_n_h_d_w_m_y['n_h_d_w_m_y'].max()))
+
